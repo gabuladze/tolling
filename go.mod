6c59f289cd5ec30a0ff2a926396514e48a69c6b1
Giorgi Abuladze <itisabu@gmail.com>
2023-12-25 18:53:19 +0400

    Added kafka producer

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/gabuladze/tolling
 go 1.21.1
 
 require (
+	github.com/confluentinc/confluent-kafka-go/v2 v2.3.0 // indirect
 	github.com/gorilla/websocket v1.5.1 // indirect
 	golang.org/x/net v0.17.0 // indirect
 )
--- receiver/main.go
+++ receiver/main.go
@@ -1,24 +1,66 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	"github.com/gabuladze/tolling/types"
 	"github.com/gorilla/websocket"
 )
 
-const listenAddr = ":3000"
+const (
+	listenAddr  = ":3000"
+	kafkaServer = "localhost"
+)
+
+var kafkaTopic = "obudata"
 
 type receiver struct {
-	msgch chan types.OBUData
-	conn  *websocket.Conn
+	prod *kafka.Producer
+	conn *websocket.Conn
 }
 
-func newReceiver() *receiver {
-	return &receiver{
-		msgch: make(chan types.OBUData, 128),
+func newReceiver() (*receiver, error) {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
+	if err != nil {
+		return nil, err
 	}
+
+	// defer p.Close()
+
+	// Delivery report handler for produced messages
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					log.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					log.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
+
+	return &receiver{
+		prod: p,
+	}, nil
+}
+
+func (rec *receiver) produceMsg(data types.OBUData) error {
+	b, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	err = rec.prod.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &kafkaTopic, Partition: kafka.PartitionAny},
+		Value:          b,
+	}, nil)
+
+	return err
 }
 
 func (rec *receiver) handleWS(w http.ResponseWriter, r *http.Request) {
@@ -45,12 +87,16 @@ func (rec *receiver) receiveLoop() {
 			continue
 		}
 		log.Printf("received OBU data from [%d]: <lat: %.2f, long: %.2f>\n", data.OBUID, data.Lat, data.Long)
-		rec.msgch <- data
+		rec.produceMsg(data)
 	}
 }
 
 func main() {
-	rec := newReceiver()
+	rec, err := newReceiver()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	http.HandleFunc("/", rec.handleWS)
 	log.Println("starting receiver at ", listenAddr)
 	if err := http.ListenAndServe(listenAddr, nil); err != nil {
