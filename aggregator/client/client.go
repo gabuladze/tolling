db50d00f5c526535c8105ba10dfdde6205d73cdc
Giorgi Abuladze <itisabu@gmail.com>
2023-12-31 18:54:43 +0400

    Added GetInvoice method to Client

--- aggregator/client/client.go
+++ aggregator/client/client.go
@@ -8,4 +8,5 @@ import (
 
 type Client interface {
 	AggregateDistance(context.Context, *types.AggregateRequest) error
+	GetInvoice(ctx context.Context, aggReq *types.GetInvoiceRequest) (*types.Invoice, error)
 }
--- aggregator/client/http_client.go
+++ aggregator/client/http_client.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -40,4 +41,28 @@ func (c *HTTPClient) AggregateDistance(ctx context.Context, aggReq *types.Aggreg
 		log.Fatalf("the service responded with non 200 status code %d", res.StatusCode)
 	}
 	return nil
+}
+
+func (c *HTTPClient) GetInvoice(ctx context.Context, aggReq *types.GetInvoiceRequest) (*types.Invoice, error) {
+	endpoint := fmt.Sprintf("%s/%s?obu=%d", c.Endpoint, "invoice", aggReq.ObuID)
+	req, err := http.NewRequest("GET", endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		log.Fatalf("the service responded with non 200 status code %d", res.StatusCode)
+	}
+
+	var inv types.Invoice
+	if err := json.NewDecoder(res.Body).Decode(&inv); err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	return &inv, nil
 }
